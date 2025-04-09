ffb26f3932d9ea3655c4a1b582e7204c26053383
Liya-Getachew <113055994+Liya-Getachew@users.noreply.github.com>
2025-04-09 10:55:38 -0400

    Update code.c

--- code.c
+++ code.c
@@ -8,18 +8,18 @@
  # include <Servo.h>
 
  Servo servoNumber1;
- const int ANALOG_PIN = 0;
+ const int ANALOG_PIN = 0; // set potentiometer pin to A0
  float angleOfServo; 
    
   void setup() 
  {
-    servoNumber1.attach(9);
+    servoNumber1.attach(9); // set servo pin to PIN 9
   }
   
   void loop() 
  {
     angleOfServo = analogRead(ANALOG_PIN);
-    angleOfServo = angleOfServo / 6; // changes potentiometer range from 0-1023 to 1-180
-    servoNumber1.write(angleOfServo);
+    angleOfServo = angleOfServo / 6; // converts potentiometer range from 0-1023 to 1-180 degrees to sync with servo
+    servoNumber1.write(angleOfServo); // tells the servo to go to the same angle as the potentiometer
  
   }
